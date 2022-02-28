94bcfedbeae686eb821bc991c3aa435bb2e877c4
A Oladipupo-Usoro <aolausoro@gmail.com>
2022-02-28 22:11:23 +0000

    corrected typo in url

--- config/index.ts
+++ config/index.ts
@@ -2,4 +2,4 @@ const dev = process.env.NODE_ENV !== "production";
 
 export const NEXT_URL = dev
   ? "http://localhost:3000"
-  : "https://viaromaninsolopizza.com";
+  : "https://viaromanonsolopizza.com";
